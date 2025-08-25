baac4f68762ee394aeefe0489234c808cf20a61c
Suhani16-K <suhanikushwaha16@gmail.com>
2025-08-26 00:58:39 +0530

    version1

---
+++ 4p.java
@@ -0,0 +1,24 @@
+public class 4p {
+    int nEmp;
+    String sName;
+    long nSal;
+    int aDesig[] = new int[5];
+    void mCalcIncomeTax(long nSal){
+        System.out.print("return income tax");
+    }
+    public static void main(String[] args){
+         
+        Employee e1 = new Employee();
+        e1.nemp = 1234;
+        e1.sName = Abhishek;
+        employee e2 = new Employee();
+         e2.nemp = 1235;
+        e2.sName = suhani;
+
+         e3.nemp = 1234;
+        e3.sName = Abhishek;
+        if(e1.nemp==e1.sName){
+            return e1.name;
+        }
+    }
+}
